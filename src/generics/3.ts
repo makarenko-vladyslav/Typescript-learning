f743944975bf84fb72b9ae73bf673dfdb8f23a14
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-05-26 20:12:25 +0200

    bugs fixed

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,3 +1,3 @@
-function merge<T extends object, U extends object>(objA: T, objB: U): T & U{
+function merge<T extends object, U extends object>(objA: T, objB: U): object {
     return Object.assign(objA, objB);
 }
